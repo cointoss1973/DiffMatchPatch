8cde7f1c64f30ee402b44240e407c4eac551ba88
Takayuki Kondo <tkondou@gmail.com>
2021-11-18 15:59:22 +0900

    Add DiffMatchPatch

---
+++ DiffMatchPatch.Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using DiffMatchPatch;
+
+// Hello World C#
+// https://github.com/google/diff-match-patch/wiki/Language:-C%23
+
+diff_match_patch dmp = new diff_match_patch();
+
+List<Diff> diff = dmp.diff_main("Hello World.", "Goodbye World.");
+// Result: [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
+
+dmp.diff_cleanupSemantic(diff);
+// Result: [(-1, "Hello"), (1, "Goodbye"), (0, " World.")]
+
+for (int i = 0; i < diff.Count; i++)
+{
+    Console.WriteLine(diff[i]);
+}
+/*
+ * Diff(DELETE,"Hello")
+ * Diff(INSERT,"Goodbye")
+ * Diff(EQUAL," World.")
+ */
---
+++ DiffMatchPatch.Speedtest/Program.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2010 Google Inc.
+// All Right Reserved.
+
+using DiffMatchPatch;
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// https://github.com/google/diff-match-patch/tree/master/csharp
+/// </summary>
+public class Program
+{
+    public static void Main(string[] args)
+    {
+            Run("Speedtest1.txt", "Speedtest2.txt");
+    }
+    public static void Run(string file1, string file2)
+    {
+        string text1 = System.IO.File.ReadAllText(file1);
+        string text2 = System.IO.File.ReadAllText(file2);
+
+        diff_match_patch dmp = new diff_match_patch();
+        dmp.Diff_Timeout = 0;
+
+        // Execute one reverse diff as a warmup.
+        dmp.diff_main(text2, text1);
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        DateTime ms_start = DateTime.Now;
+        List<Diff> diffs = dmp.diff_main(text1, text2);
+        DateTime ms_end = DateTime.Now;
+
+        //foreach(var diff in diffs)
+        //{
+        //    Console.WriteLine(diff);
+        //}
+        Console.WriteLine("Elapsed time: " + (ms_end - ms_start));
+    }
+}
